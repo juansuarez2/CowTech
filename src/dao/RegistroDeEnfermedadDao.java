c6d7d34e28df4cc565bd6e4bde8009e2ef92ccf2
juansuarez2 <juan.suarez@estudiantes.utec.edu.uy>
2025-09-11 23:07:55 -0300

    Arreglo de sintaxis

--- src/dao/RegistroDeEnfermedadDao.java
+++ src/dao/RegistroDeEnfermedadDao.java
@@ -10,7 +10,7 @@ import database.DatabaseConnection;
 public class RegistroDeEnfermedadDao {
     public void crearRegistroEnfermedad(RegistroEnfermedad registro) throws SQLException{
         String sql = "INSERT INTO (id_enfermedad, id_animal, fechaInicio, fechaFinal, estado) VALUES (?, ?, ?, ?, ?)";
-        try (PreparedStatement ps = DatabaseConnection.getInstacia().getConnection().prepareStatement(sql)){
+        try (PreparedStatement ps = DatabaseConnection.getInstancia().getConnection().prepareStatement(sql)){
             ps.setInt(1, registro.getIdEnfermedad());
             ps.setInt(2, registro.getIdAnimal());
             ps.setDate(3, registro.getFechaInicio());
@@ -24,7 +24,7 @@ public class RegistroDeEnfermedadDao {
     public List<RegistroEnfermedad> listarRegistroEnfermedad() throws SQLException{
         List<RegistroEnfermedad> lista = new ArrayList<>();
         String sql = "SELECT * FROM registroEnfermedad";
-        try (Statement st = DatabaseConnection.getInstacia().getConnection().createStatement();
+        try (Statement st = DatabaseConnection.getInstancia().getConnection().createStatement();
              ResultSet rs = st.executeQuery(sql)) {
             while (rs.next()) {
                 lista.add(new RegistroEnfermedad(rs.getInt("id"), rs.getInt("id_enfermedad"), rs.getInt("id_animal"), rs.getDate("fechaInicio"), rs.getDate("fechaFinal"), rs.getBoolean("estado") ));
@@ -35,7 +35,7 @@ public class RegistroDeEnfermedadDao {
 
     public void editarRegistroEnfermedad(int id, int newEnfermedad, int newAnimal, Date newFechaINI, Date newFechaFIN, boolean newEstado) throws SQLException{
         String sql = "UPDATE registroEnfermedad SET id_enfermedad = ?, id_animal = ?, fechaInicio = ?, fechaFinal = ?, estado = ? WHERE id = ?";
-        try (PreparedStatement ps = DatabaseConnection.getInstacia().getConnection().prepareStatement(sql)) {
+        try (PreparedStatement ps = DatabaseConnection.getInstancia().getConnection().prepareStatement(sql)) {
             ps.setInt(1, newEnfermedad);
             ps.setInt(2, newAnimal);
             ps.setDate(3, newFechaINI);
@@ -50,7 +50,7 @@ public class RegistroDeEnfermedadDao {
 
     public void borrarRegistroEnfermedad(int id) throws SQLException {
         String sql = "DELETE FROM registroEnfermedad WHERE id = ?";
-        try (PreparedStatement ps = DatabaseConnection.getInstacia().getConnection().prepareStatement(sql)) {
+        try (PreparedStatement ps = DatabaseConnection.getInstancia().getConnection().prepareStatement(sql)) {
             ps.setInt(1, id);
             int filas = ps.executeUpdate();
             if (filas > 0) System.out.println("✅ Registro de Enfermedad eliminado.");
